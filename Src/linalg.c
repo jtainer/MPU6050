8c3ebd5c62a2654dd73c8dce133ef5120be44a2a
jtainer <jonathantainer@yahoo.com>
2023-12-05 05:51:48 -0500

    Expanded vec3 functions and refactored quaternion functions

--- Src/linalg.c
+++ Src/linalg.c
@@ -7,15 +7,15 @@
 #include "linalg.h"
 #include <math.h>
 
-vec4 quaternion_add(vec4 a, vec4 b) {
+vec4 vec4_add(vec4 a, vec4 b) {
 	return (vec4) { a.w+b.w, a.x+b.x, a.y+b.y, a.z+b.z };
 }
 
-vec4 quaternion_subtract(vec4 a, vec4 b) {
+vec4 vec4_subtract(vec4 a, vec4 b) {
 	return (vec4) { a.w-b.w, a.x-b.x, a.y-b.y, a.z-b.z };
 }
 
-vec4 quaternion_multiply(vec4 a, vec4 b) {
+vec4 vec4_multiply(vec4 a, vec4 b) {
 	vec4 res = { 0, 0, 0, 0 };
 	res.w = a.w*b.w - a.x*b.x - a.y*b.y - a.z*b.z;
 	res.x = a.w*b.x + a.x*b.w + a.y*b.z - a.z*b.y;
@@ -24,48 +24,71 @@ vec4 quaternion_multiply(vec4 a, vec4 b) {
 	return res;
 }
 
-vec4 quaternion_scale(vec4 a, float scale) {
+vec4 vec4_scale(vec4 a, float scale) {
 	return (vec4) { a.w*scale, a.x*scale, a.y*scale, a.z*scale };
 }
 
-float quaternion_length(vec4 a) {
+float vec4_length(vec4 a) {
 	return sqrtf(a.w*a.w + a.x*a.x + a.y*a.y + a.z*a.z);
 }
 
-vec4 quaternion_normalize(vec4 a) {
+vec4 vec4_normalize(vec4 a) {
 	float inv_len = 1.f / quaternion_length(a);
-	return quaternion_scale(a, inv_len);
+	return vec4_scale(a, inv_len);
 }
 
-vec4 quaternion_conjugate(vec4 a) {
+vec4 vec4_conjugate(vec4 a) {
 	return (vec4) { a.w, -a.x, -a.y, -a.z };
 }
 
-vec4 quaternion_inverse(vec4 a) {
+vec4 vec4_inverse(vec4 a) {
 	float norm = quaternion_length(a);
 	float scale = 1.f / (norm*norm);
 	vec4 conj = quaternion_conjugate(a);
 	return quaternion_scale(conj, scale);
 }
 
-float vector3_dot(vec3 a, vec3 b) {
-	return a.x*b.x + a.y*b.y + a.z*b.z;
-}
-
-vec3 vector3_cross(vec3 a, vec3 b) {
-	return (vec3) { a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x };
-}
-
-vec4 quaternion_rotation(vec3 from, vec3 to) {
+vec4 vec4_rotation(vec3 from, vec3 to) {
     vec4 result = { 0 };
 
-    float dot = vector3_dot(from, to);
-    vec3 cross = vector3_cross(from, to);
+    float dot = vec3_dot(from, to);
+    vec3 cross = vec3_cross(from, to);
 
     result.x = cross.x;
     result.y = cross.y;
     result.z = cross.z;
     result.w = 1.f + dot;
 
-    return quaternion_normalize(result);
+    return vec4_normalize(result);
 }
+
+vec3 vec3_add(vec3 a, vec3 b) {
+	return (vec3) { a.x+b.x, a.y+b.y, a.z+b.z };
+}
+
+vec3 vec3_subtract(vec3 a, vec3 b) {
+	return (vec3) { a.x-b.x, a.y-b.y, a.z-b.z };
+}
+
+vec3 vec3_scale(vec3 a, float scale) {
+	return (vec3) { a.x*scale, a.y*scale, a.z*scale };
+}
+
+float vec3_length(vec3 a) {
+	return sqrtf(a.x*a.x + a.y*a.y + a.z*a.z);
+}
+
+vec3 vec3_normalize(vec3 a) {
+	float inv_len = 1.f / vec3_length(a);
+	return vec3_scale(a, inv_len);
+}
+
+float vec3_dot(vec3 a, vec3 b) {
+	return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
+vec3 vec3_cross(vec3 a, vec3 b) {
+	return (vec3) { a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x };
+}
+
+
--- Src/linalg.h
+++ Src/linalg.h
@@ -15,23 +15,33 @@ typedef struct vec3 {
 	float x, y, z;
 } vec3;
 
-vec4 quaternion_add(vec4 a, vec4 b);
+vec4 vec4_add(vec4 a, vec4 b);
 
-vec4 quaternion_subtract(vec4 a, vec4 b);
+vec4 vec4_subtract(vec4 a, vec4 b);
 
-vec4 quaternion_multiply(vec4 a, vec4 b);
+vec4 vec4_multiply(vec4 a, vec4 b);
 
-vec4 quaternion_scale(vec4 a, float scale);
+vec4 vec4_scale(vec4 a, float scale);
 
-float quaternion_length(vec4 a);
+float vec4_length(vec4 a);
 
-vec4 quaternion_normalize(vec4 a);
+vec4 vec4_normalize(vec4 a);
 
-vec4 quaternion_conjugate(vec4 a);
+vec4 vec4_conjugate(vec4 a);
 
-vec4 quaternion_inverse(vec4 a);
+vec4 vec4_inverse(vec4 a);
 
-vec4 quaternion_rotation(vec3 from, vec3 to);
+vec4 vec4_rotation(vec3 from, vec3 to);
+
+vec3 vec3_add(vec3 a, vec3 b);
+
+vec3 vec3_subtract(vec3 a, vec3 b);
+
+vec3 vec3_scale(vec3 a, float scale);
+
+float vec3 length(vec3 a);
+
+vec3 vec3_normalize(vec3 a);
 
 float vector3_dot(vec3 a, vec3 b);
 
